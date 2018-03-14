94931c62c005619366a5a861776fdcf151c5664b
Rhenan Bartels <rhenan.bartels@gmail.com>
2018-03-14 14:45:56 -0300

    Generaliza a funcao parse e adiciona mais um processo aos testes

--- crawler/parser.py
+++ crawler/parser.py
@@ -1,39 +1,62 @@
-import re
+from slugify import slugify
 
-from bs4 import BeautifulSoup
+from robotj.crawler.utils import limpa_conteudo
 
 
-def parse_metadados(html, numero_processo):
-    soup = BeautifulSoup(html, 'html.parser')
-    linhas = soup.find_all('td')
+def parse_metadados(linhas_de_dados, numero_processo, inicio_metadados,
+                    fim_metadados):
     metadados = {
-        'status': '',
-        'comarca': '',
-        'comarca_info_1': '',
-        'comarca_info_2': '',
-        'endereco': '',
-        'bairro': '',
-        'cidade': '',
-        'acao': '',
-        'assunto': '',
-        'classe': '',
-        'aviso_ao_advogado': '',
-        'autor': '',
-        'requerido': '',
-        'advogado': ''
+        'status': [''],
+        'endereco': [''],
+        'bairro': [''],
+        'cidade': [''],
+        'acao': [''],
+        'assunto': [''],
+        'classe': [''],
+        'aviso_ao_advogado': [''],
+        'autor': [''],
+        'requerido': [''],
+        'requerente': [''],
+        'advogado-s': ['']
     }
-    pares = [
-        ('status', 6), ('comarca', 8), ('comarca_info_1', 9),
-        ('comarca_info_2', 11), ('endereco', 14), ('bairro', 16),
-        ('cidade', 18), ('acao', 21), ('assunto', 24), ('classe', 27),
-        ('autor', 30), ('requerido', 32), ('advogado', 35)
-    ]
+
+    # Delimita o processo na regiao dos metadados
+    linhas_com_metadados = linhas_de_dados[inicio_metadados:fim_metadados]
 
     metadados['numero_processo'] = numero_processo
+    metadados['status'] = limpa_conteudo(
+        linhas_com_metadados[0].find_all('td')[0].get_text()
+    )
 
-    for chave, indice in pares:
-        metadados[chave] = re.sub(
-            '\s+', ' ', linhas[indice].get_text()).strip()
+    del linhas_com_metadados[:2]
+    dados_comarca = []
+    for tr in list(linhas_com_metadados):
+        linhas_com_metadados.pop(0)
+        if len(tr.find_all('td')) == 1:
+            break
+        dados_comarca += [tr]
+
+    # TODO: refatorar
+    textos = []
+    for tr in dados_comarca:
+        for td in tr.find_all('td'):
+            textos += list(
+                filter(None, [limpa_conteudo(td.get_text()) if td else ''])
+            )
+
+    metadados['comarca'] = textos
+
+    for tr in list(linhas_com_metadados):
+        linhas_com_metadados.pop(0)
+        linha = []
+        tds = tr.find_all('td')
+        for td in tds:
+            linha += list(
+                filter(None, [limpa_conteudo(td.get_text()) if td else ''])
+            )
+
+            if linha:
+                metadados[slugify(linha[0])] = linha[1:]
 
     return metadados
 
